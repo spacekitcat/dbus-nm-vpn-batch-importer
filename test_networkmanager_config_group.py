7dfbf0c513650a693c98928ab54d8461d34b7f66
lisaplusplus <0lisa.burton@gmail.com>
2017-10-22 02:39:35 +0100

    Removing dead tests and fixing an incorrect file extension.

--- test_networkmanager_config_group.py
+++ test_networkmanager_config_group.py
@@ -21,39 +21,5 @@ class TestDefaultStateNetworkManagerNetworkManagerConnectionConfigGroup(unittest
   def test_assertthat__unknown_key__throws_exception(self):
     self.assertRaises(KeyError, self.sut.get_value, 'this_key_does_not_exist')
 
-#class TestObjectUpdatesNetworkManagerNetworkManagerConnectionConfigGroup(unittest.TestCase):
-#  def setUp(self):
-#    self.sut = NetworkManagerConnectionConfigGroup( { 'obj_value' : { 'child_int': 0, 'child_str': 'hi!' } } )
-#
-#  def test_assertthat__update_objval_blankobj__equals_original(self):
-#    self.sut.set_value('obj_value', {} )
-#    result = self.sut.get_value('obj_value')
-#    self.assertEqual( { 'child_int': 0, 'child_str': 'hi!' }, result)
-#
-#  def test_assertthat__update_objval_firstkey_match__matchedkey_updated(self):
-#    self.sut.set_value('obj_value', { 'child_int': 5 } )
-#    result = self.sut.get_value('obj_value')
-#    self.assertEqual( { 'child_int': 5, 'child_str': 'hi!' }, result)
-#
-#  def test_assertthat__update_objval_secondkey_match__matchedkey_updated(self):
-#    self.sut.set_value('obj_value', { 'child_str': 'bye!' } )
-#    result = self.sut.get_value('obj_value')
-#    self.assertEqual( { 'child_int': 0, 'child_str': 'bye!' }, result)
-#
-#  def test_assertthat__update_objval_bothkey_match__matchedkey_updated(self):
-#    self.sut.set_value('obj_value', { 'child_int': 3, 'child_str': 'Buhbye!' } )
-#    result = self.sut.get_value('obj_value')
-#    self.assertEqual( { 'child_int': 3, 'child_str': 'Buhbye!' }, result)
-#
-#  def test_assertthat__update_objval_newkey__newkey_rejected(self):
-#    self.sut.set_value('obj_value', { 'new_child': 7 } )
-#    result = self.sut.get_value('obj_value')
-#    self.assertEqual( { 'child_int': 0, 'child_str': 'hi!' }, result)
-#
-#  def test_assertthat__update_objval_newkeys_and_matched_key__newkey_rejected_added_mathedkeys_updated(self):
-#    self.sut.set_value('obj_value', { 'child_str': 'Oh ya?','new_child': 7 } )
-#    result = self.sut.get_value('obj_value')
-#    self.assertEqual( { 'child_int': 0, 'child_str': 'Oh ya?' }, result)
-#
 if __name__ == '__main__':
   unittest.main()
